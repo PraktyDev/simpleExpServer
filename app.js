fdbae7d401ce8479c839435ca77bbdc5af781680
Praise Akintayo <prakty1205@gmail.com>
2024-07-02 02:32:10 -0700

    changed and added a new endpoint

--- app.js
+++ app.js
@@ -5,6 +5,15 @@ const dotenv = require('dotenv').config()
 const app = express()
 async function getLocation(){
     try{
+        const response = await fetch(`https://ip-geolocation.whoisxmlapi.com/api/v1?apiKey=${process.env.API_SECRET}`)
+        const data = await response.json()
+        return data
+    }catch(err){
+        console.error(err)
+    }
+}
+async function getTemp(){
+    try{
         const response = await fetch(`http://api.weatherapi.com/v1/current.json?key=${process.env.WEATHER_API_KEY}&q=auto:ip`)
         const data = await response.json()
         return data
@@ -15,12 +24,14 @@ async function getLocation(){
 
 app.get("/api/hello", async (req, res) => {
     const visitor = req.query.visitor_name
-    const client_ip = ip.address()
 
     try{
         const locale = await getLocation()
         const city = locale.location.region
-        const temp = locale.current.temp_c
+        const client_ip = locale.ip
+
+        const temperature = await getTemp()
+        const temp = temperature.current.temp_c
 
         const greeting = `Hello, ${visitor}!, the temperature is ${temp} degree Celcius in ${city}`;
 
